26740e669f97da3c66c77a8b9f60ec887144de88
Yuling Liang <yuling.code@gmail.com>
2014-02-11 00:23:26 -0800

    Added service status monitoring.

--- app/src/main/java/com/yulingliang/app/android/PhoneUsages/MainActivity.java
+++ app/src/main/java/com/yulingliang/app/android/PhoneUsages/MainActivity.java
@@ -1,11 +1,15 @@
 package com.yulingliang.app.android.PhoneUsages;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +17,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -52,15 +58,28 @@ public class MainActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        private static final String TAG = "PhoneUsages.PlaceholderFragment";
         private View mRootView;
+        private SharedPreferences mPref;
 
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             mRootView = inflater.inflate(R.layout.fragment_main, container, false);
+            mPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+            mPref.registerOnSharedPreferenceChangeListener(
+                    new SharedPreferences.OnSharedPreferenceChangeListener() {
+                        @Override
+                        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                                              String key) {
+                            Log.d(TAG, "SharedPref has changed for key: " + key);
+                            updateStats();
+                        }
+                    });
 
             Button startButton = (Button) mRootView.findViewById(R.id.start_button);
             startButton.setOnClickListener(new View.OnClickListener() {
@@ -80,8 +99,19 @@ public class MainActivity extends ActionBarActivity {
                 }
             });
 
+            Button resetButton = (Button) mRootView.findViewById(R.id.reset_stat_button);
+            resetButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mPref.edit()
+                            .putInt(Constants.PREF_NUM_SCREEN_ON, 0)
+                            .putInt(Constants.PREF_NUM_UNLOCK, 0)
+                            .commit();
+                    updateStats();
+                }
+            });
 
-            updateStats(mRootView);
+            updateStats();
 
             return mRootView;
         }
@@ -89,22 +119,41 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public void onResume() {
             super.onResume();
-            updateStats(mRootView);
+            updateStats();
         }
 
-        private void updateStats(View rootView) {
-            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
-            int numScreenOn = pref.getInt(Constants.PREF_NUM_SCREEN_ON, 0);
-            int numUnlock = pref.getInt(Constants.PREF_NUM_UNLOCK, 0);
+        private void updateStats() {
+            int numScreenOn = mPref.getInt(Constants.PREF_NUM_SCREEN_ON, 0);
+            int numUnlock = mPref.getInt(Constants.PREF_NUM_UNLOCK, 0);
 
-            TextView numScreenOnView = (TextView) rootView.findViewById(R.id.num_screen_on);
+            TextView numScreenOnView = (TextView) mRootView.findViewById(R.id.num_screen_on);
             numScreenOnView.setText(getString(R.string.num_screen_on, numScreenOn));
 
-            TextView numUnlockView = (TextView) rootView.findViewById(R.id.num_actual_use);
+            TextView numUnlockView = (TextView) mRootView.findViewById(R.id.num_actual_use);
             numUnlockView.setText(getString(R.string.num_unlock, numUnlock));
+
+            TextView serviceStatus = (TextView) mRootView.findViewById(R.id.service_status);
+            if(isListenerServiceRunning()) {
+                serviceStatus.setTextColor(Color.GREEN);
+                serviceStatus.setText(getString(R.string.service_running));
+            } else {
+                serviceStatus.setTextColor(Color.RED);
+                serviceStatus.setText(getString(R.string.service_stopped));
+            }
         }
 
-
+        private boolean isListenerServiceRunning() {
+            ActivityManager manager = (ActivityManager) getActivity().getSystemService(Context
+                    .ACTIVITY_SERVICE);
+            for (ActivityManager.RunningServiceInfo service : manager
+                    .getRunningServices(Integer.MAX_VALUE)) {
+                if (ScreenStatusListenerService.class.getName()
+                        .equals(service.service.getClassName())) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
 }
